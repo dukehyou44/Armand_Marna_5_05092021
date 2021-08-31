be0b7bb97a5721edd69b2fa517d32a884e687470
Armand Marna <azor1898@gmail.com>
2021-08-31 11:56:15 +0200

    first commit front-end

---
+++ frontend/index.js
@@ -0,0 +1,39 @@
+
+(async function(){
+    const articles = await getArticles()
+    
+    for(article of articles) {
+      displayArticles(articles)
+    }
+})()
+
+function getArticles() {
+    return fetch("http://localhost:3000/api/furniture")
+    .then(function(data) {
+        return data.json()
+    })
+    .then(function(articles) {
+        return articles
+    })
+    .catch(function(err) {
+        console.log("erreur : " + err)
+    })
+}
+
+function displayArticles() {
+    document.querySelector(".container").innerHTML += 
+    `<div class>
+        <div class="carte article">
+            <img src="${article.imageUrl}" alt="${article.name}">
+        </div>
+        <div class="carte infos">
+            <div class="carte titre et prix">
+                <h2>${article.name}</h2>
+                <p>${article.price / 100}.00 €</p>
+            </div>
+            <div class="carte description">
+                <p>${article.description}</p>
+            </div>
+        </div>
+    </div>`
+}    
